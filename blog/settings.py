b9c005fe161b5169b69c1b1f0e2bb931f9f35f30
ekarat <ekarat.9097@gmail.com>
2024-04-07 13:11:24 +0700

    migrate from sqllite to postgresql

--- blog/settings.py
+++ blog/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
     }
 }
 
